c983476cd040f2919d8808dc67578411d14494a7
VARDAAN08 <163497051+VARDAAN08@users.noreply.github.com>
2024-03-16 18:07:51 +0530

    intials

---
+++ script.js
@@ -0,0 +1,10 @@
+var a=document.querySelector('#mybutton').textContent;
+var b=document.querySelector('#mybutton');
+var c=document.querySelector('h1');
+b.addEventListner("click",function(){
+    c.innerHTML="yoooo";
+
+    
+    
+    
+})
